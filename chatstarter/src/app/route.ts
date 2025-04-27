b3fc632d9893947d282e97ffe809282df4868aad
Ashan Deen <ash474d@gmail.com>
2025-04-27 19:19:50 -0400

    s

--- chatstarter/src/app/route.ts
+++
@@ -1,8 +0,0 @@
-// // src/app/route.ts
-// import { NextResponse } from "next/server";
-
-// export function GET(request: Request) {
-//   const url = new URL(request.url);
-//   url.pathname = "/dms";
-//   return NextResponse.redirect(url);
-// }
